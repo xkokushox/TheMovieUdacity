1f84400a1bff30a0e6c9da1cee9e940d9ca98c89
Jose Torres <jose.torres@citi.com.mx>
2016-11-11 10:13:01 -0600

    4.20_projections

---
+++ PopularMovies/app/src/androidTest/java/com/freakybyte/movies/data/tables/TestMovieContract.java
@@ -0,0 +1,39 @@
+package com.freakybyte.movies.data.tables;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+import org.junit.Test;
+
+/**
+ * Created by Jose Torres on 10/11/2016.
+ */
+
+public class TestMovieContract extends AndroidTestCase {
+
+    private static final long TEST_MOVIE_ID = 271110;
+
+    @Test
+    public void testBuildMovieId() throws Exception {
+        Uri movieUri = MovieEntry.buildMovieUri(TEST_MOVIE_ID);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                "WeatherContract.", movieUri);
+        assertEquals("Error: Movie id not properly appended to the end of the Uri",
+                String.valueOf(TEST_MOVIE_ID), movieUri.getLastPathSegment());
+        assertEquals("Error: Movie id Uri doesn't match our expected result",
+                movieUri.toString(),
+                "content://com.freakybyte.movies/movie/" + TEST_MOVIE_ID);
+    }
+
+    @Test
+    public void testBuildMovieId() throws Exception {
+        Uri movieUri = MovieEntry.buildMovieUri(TEST_MOVIE_ID);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                "WeatherContract.", movieUri);
+        assertEquals("Error: Movie id not properly appended to the end of the Uri",
+                String.valueOf(TEST_MOVIE_ID), movieUri.getLastPathSegment());
+        assertEquals("Error: Movie id Uri doesn't match our expected result",
+                movieUri.toString(),
+                "content://com.freakybyte.movies/movie/" + TEST_MOVIE_ID);
+    }
+}
